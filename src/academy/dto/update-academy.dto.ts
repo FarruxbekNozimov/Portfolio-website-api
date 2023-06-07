79612b3aebbe06ac5c9dd556dbdb4c9e1720fb9a
FarruxDEV <farruxbeknozimov@gmail.com>
2023-06-07 16:57:20 +0500

    add academy update dto

--- src/academy/dto/update-academy.dto.ts
+++ src/academy/dto/update-academy.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateAcademyDto {
-  
+  @ApiProperty({ example: "Najot Ta'lim" })
+  name?: string;
+
+  @ApiProperty({ example: '2021-2023' })
+  time?: string;
+
+  @ApiProperty({ example: 'NodeJS + VudeJS FullStack' })
+  description?: string;
 }
