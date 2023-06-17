d19789c693afb87621ce90670b4dd6df6172643b
FarruxDEV <farruxbeknozimov@gmail.com>
2023-06-17 20:34:43 +0500

    change in work

--- src/work/dto/update-work.dto.ts
+++ src/work/dto/update-work.dto.ts
@@ -1,3 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateWorkDto {
-  
+  @ApiProperty({ example: 'Biomedics' })
+  name?: string;
+
+  @ApiProperty({ example: 'Andijon shahar Bo`ston tumani' })
+  address?: string;
+
+  @ApiProperty({ example: '2022.01.01' })
+  start_time?: string;
+
+  @ApiProperty({ example: '2023.01.04' })
+  end_time?: string;
 }
--- src/work/schemas/work.schema.ts
+++ src/work/schemas/work.schema.ts
@@ -6,21 +6,16 @@ export type WorkDocument = HydratedDocument<Work>;
 @Schema({ timestamps: true })
 export class Work {
   @Prop()
-	name:string;
+  name: string;
 
-	@Prop()
-	address:string;
+  @Prop()
+  address: string;
 
-	@Prop()
-	img:string;
+  @Prop()
+  start_time: string;
 
-	@Prop()
-	start_time:string;
-
-	@Prop()
-	end_time:string;
-
-	;
+  @Prop()
+  end_time: string;
 }
 
 export const WorkSchema = SchemaFactory.createForClass(Work);
